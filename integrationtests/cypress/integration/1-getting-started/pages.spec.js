212a989728ae4afbe06698b577ed33255b0ece02
Rúni Vørmadal <runivormadal@gmail.com>
2022-06-20 21:09:42 +0200

    cleanup

--- integrationtests/cypress/integration/1-getting-started/pages.spec.js
+++ integrationtests/cypress/integration/1-getting-started/pages.spec.js
@@ -10,7 +10,7 @@
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
-const identifiers = {
+const ids = {
   treeNodeLabel: '[data-cy=tree-node-label]',
   contextMenuItem: '[data-cy=tree-node-context-menu-item]',
   dialogInput: '[data-cy=dialog-input]',
@@ -19,67 +19,33 @@ const identifiers = {
   deleteBtn: '[data-cy=delete-btn]'
 }
 
-const BASE_URL = Cypress.env('CY_BASE_URL') || 'http://localhost:4000';
 describe('Pages', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit(BASE_URL);
-    cy.get('#email').type(Cypress.env('CY_TEST_USER_EMAIL') || 'admin@vormadal.com');
-    cy.get('#password').type(Cypress.env('CY_TEST_USER_PASSWORD') || 'example');
-    
-    cy.get('#login-btn').click();
+
+    cy.login();
+    cy.visit('/')
   })
 
   it('Root folder should exist', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get(identifiers.treeNodeLabel).should('have.length', 1);
-    cy.get(identifiers.treeNodeLabel).first().should('have.text', "Root");
+    cy.get(ids.treeNodeLabel).should('have.length', 1);
+    cy.get(ids.treeNodeLabel).first().should('have.text', "Root");
   })
 
 
   it('Create new Page in root', () => {
-    // We'll store our item text in a variable so we can reuse it
     const pageName = 'Why is this so easy?';
 
     cy.intercept('POST', '/pages').as('createPage');
     cy.wait(1000); //wait for async re-render
-    cy.get(identifiers.treeNodeLabel).should('have.length', 1)
+    cy.get(ids.treeNodeLabel).should('have.length', 1)
 
-    cy.get(identifiers.treeNodeLabel).rightclick();    
-    cy.get(identifiers.contextMenuItem).should('have.length', 3);
-    cy.get(identifiers.contextMenuItem).first().should('have.text', 'Add Page');
-    cy.get(identifiers.contextMenuItem).first().click();
+    cy.get(ids.treeNodeLabel).rightclick();    
+    cy.get(ids.contextMenuItem).should('have.length', 3);
+    cy.get(ids.contextMenuItem).first().should('have.text', 'Add Page');
+    cy.get(ids.contextMenuItem).first().click();
 
-    cy.get(identifiers.dialogInput).type(pageName);
-    cy.get(identifiers.dialogOk).click();
-
-    cy.wait('@createPage').its('request.body').should()
+    cy.get(ids.dialogInput).type(pageName);
+    cy.get(ids.dialogOk).click();
   })
 
-  it('Navigate to page', () => {
-
-
-  })
-  context('with a checked task', () => {
-    beforeEach(() => {
-
-    })
-
-    it('can filter for uncompleted tasks', () => {
-
-    })
-
-    it('can filter for completed tasks', () => {
-
-    })
-
-    it('can delete all completed tasks', () => {
-
-    })
-  })
 })
--- integrationtests/cypress/support/commands.js
+++ integrationtests/cypress/support/commands.js
@@ -10,18 +10,15 @@
 //
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => { 
-    
- })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (email = 'admin@vormadal.com', password = 'example') => {
+
+    cy.request({
+        url: '/auth/login',
+        method: 'POST',
+        body: { email, password }
+    })
+        .then(response => {
+            console.log('response', response.body);
+            cy.window().its('localStorage').invoke('setItem', 'jwt', response.body.jwt);
+        })
+})
